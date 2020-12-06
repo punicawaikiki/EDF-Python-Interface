90a256a0a9ab023505004fdc8f1d7cfa47eebc08
Fabian Wicker <fabian.wicker@outlook.com>
2020-12-06 20:36:59 +0100

    started with updating gui

--- Send.py
+++ Send.py
@@ -1,6 +1,6 @@
 import numpy as np
 import socket
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plotter
 from time import sleep
 import struct
 
@@ -11,7 +11,7 @@ udp_struct_size = 64
 
 # How many time points are needed i,e., Sampling Frequency
 
-samplingFrequency   = 100
+samplingFrequency   = 102.4
 
  
 
@@ -29,7 +29,7 @@ beginTime           = 0
 
 # End time period of the signals
 
-endTime             = 10.24 
+endTime             = 10 
 
  
 
@@ -60,10 +60,13 @@ amplitude1 = np.sin(2*np.pi*signal1Frequency*time)
 # fourierTransform = fourierTransform[range(int(len(amplitude1)/2))] # Exclude sampling frequency
 
  
-
+ 
 # tpCount     = len(amplitude1)
 
+
+
 # values      = np.arange(int(tpCount/2))
+
 
 # timePeriod  = tpCount/samplingFrequency
 
@@ -76,14 +79,48 @@ UDP_PORT = 55556
 cnt = 0
 epoch_amplitude1 = np.array_split(amplitude1, 16)
 epoch_time = np.array_split(time, 16)
+
+# # Create subplot
+# figure, axis = plotter.subplots(2, 1)
+# plotter.subplots_adjust(hspace=1)
+
+# # Time domain representation for sine wave 1
+
+# axis[0].set_title('Sine wave with a frequency of 4 Hz')
+# axis[0].plot(time, amplitude1)
+# axis[0].set_xlabel('Time')
+# axis[0].set_ylabel('Amplitude')
+
+# # Frequency domain representation
+
+# fourierTransform = np.fft.fft(amplitude1)/len(amplitude1)           # Normalize amplitude
+
+# fourierTransform = fourierTransform[range(int(len(amplitude1)/2))] # Exclude sampling frequency
+
+# tpCount     = len(amplitude1)
+# values      = np.arange(int(tpCount/2))
+# timePeriod  = tpCount/samplingFrequency
+# frequencies = values/timePeriod
+
+# test = abs(fourierTransform)
+
+# # Frequency domain representation
+
+# axis[1].set_title('Fourier transform depicting the frequency components')
+# axis[1].plot(frequencies, abs(fourierTransform))
+# axis[1].set_xlabel('Frequency')
+# axis[1].set_ylabel('Amplitude')
+
+# plotter.show()
+
+
 while(True):
-    for i in range(0, int(len(time) / udp_struct_size)):
+    for messageNumber in range(0, int(len(time) / udp_struct_size)):
         sock = socket.socket(socket.AF_INET,
                         socket.SOCK_DGRAM) #UDP
-        sample_struct = struct.pack('64d 64d', *epoch_time[i], *epoch_amplitude1[i])
+        sample_struct = struct.pack('1i 64d 64d', messageNumber, *epoch_time[messageNumber], *epoch_amplitude1[messageNumber])
         sock.sendto(sample_struct, (UDP_IP, UDP_PORT))
-        print(sample_struct)
-        print(struct.unpack('64d 64d', sample_struct))
+        print(struct.unpack('1i 64d 64d', sample_struct))
         cnt += 1
         sleep(0.1)
     print('Sample finshed to send')
